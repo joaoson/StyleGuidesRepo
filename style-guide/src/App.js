0e2429e2351414a1ebd74b6617309bca6585a4e0
Joao Freitas <joaovitor6586@gmail.com>
2024-01-30 09:07:54 -0300

    feat: added test

--- style-guide/src/App.js
+++ style-guide/src/App.js
@@ -4,7 +4,7 @@ import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
+    <div id='app' className="App">
       <Dashboard title="John's Dashboard" />
     </div>
   );
